e5059716cde6cd97399229aec9649d3ee6c01343
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-09-20 03:29:40 +0530

    FileReader : a project to read file on another thread.

---
+++ FileReader/Service/FileService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Service
+{
+    public class FileService
+    {
+        private readonly string filePath;
+
+        public FileService()
+        {
+            filePath = Path.Combine(
+                Directory.GetParent(
+                    AppDomain.CurrentDomain.BaseDirectory
+                )!.Parent!.Parent!.Parent!.FullName,
+                "data.txt"
+            );
+        }
+
+        public bool FileExists()
+        {
+            return File.Exists(filePath);
+        }
+
+        public string ReadFile()
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public DateTime GetLastModified()
+        {
+            return File.GetLastWriteTime(filePath);
+        }
+    }
+}
---
+++ FileReader/ViewModel/MainViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Service;
+using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly FileService fileService;
+
+        private string outputText = "";
+        private string status = "";
+
+        private Thread? fileThread;
+        private DateTime lastModified;
+
+        public string OutputText
+        {
+            get => outputText;
+            set
+            {
+                outputText = value;
+                OnPropertyChanged(nameof(OutputText));
+            }
+        }
+
+        public string Status
+        {
+            get => status;
+            set
+            {
+                status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        public RelayCommand ReadFileCommand { get; }
+
+        public MainViewModel()
+        {
+            fileService = new FileService();
+
+            ReadFileCommand = new RelayCommand(ReadFile);
+
+            if (fileService.FileExists())
+            {
+                lastModified = fileService.GetLastModified();
+            }
+
+            StartFileThread();
+        }
+
+        private void ReadFile()
+        {
+            if (fileService.FileExists())
+            {
+                OutputText = fileService.ReadFile();
+                Status = "File read successfully.";
+            }
+            else
+            {
+                OutputText = "";
+                Status = "data.txt file was not found!";
+            }
+        }
+
+        private void StartFileThread()
+        {
+            fileThread = new Thread(CheckFile);
+            fileThread.IsBackground = true;
+            fileThread.Start();
+        }
+
+        private void CheckFile()
+        {
+            while (true)
+            {
+                if (fileService.FileExists())
+                {
+                    DateTime currentModified =
+                        fileService.GetLastModified();
+
+                    if (currentModified != lastModified)
+                    {
+                        lastModified = currentModified;
+                    }
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(propertyName)
+            );
+        }
+    }
+}
